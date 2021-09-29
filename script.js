6af7f4fa8af37107ba3444e8159d2121a7972057
Satish maurya <satishmaurya15216@gmail.com>
2021-09-29 10:36:30 +0530

    header is updated on website

---
+++ script.js
@@ -0,0 +1,12 @@
+let searchbtn = document.getElementById("search-btn")
+let searchbar = document.querySelector(".search-bar-container")
+
+window.onscroll=()=>{
+    searchbtn.classList.remove("fa-times")
+    searchbar.classList.remove("active")
+}
+
+searchbtn.addEventListener('click',()=>{
+    searchbtn.classList.toggle("fa-times")
+    searchbar.classList.toggle("active")
+})
