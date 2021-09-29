0ef2fdc1c4cb3e7fa58b71bd957d4df6f817a2be
Satish maurya <satishmaurya15216@gmail.com>
2021-09-29 15:34:18 +0530

    updating the navbar with form section and toggle menu

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let searchbtn = document.getElementById("search-btn")
+let loginbtn = document.getElementById("login-btn")
 let searchbar = document.querySelector(".search-bar-container")
+let loginform = document.querySelector(".login-form-container")
+let closeform = document.getElementById("form-close")
+let menubar = document.getElementById("menu-bar")
+let navbar = document.querySelector(".navbar")
+
 
 window.onscroll=()=>{
     searchbtn.classList.remove("fa-times")
@@ -10,3 +16,17 @@ searchbtn.addEventListener('click',()=>{
     searchbtn.classList.toggle("fa-times")
     searchbar.classList.toggle("active")
 })
+
+loginbtn.addEventListener('click',()=>{
+    loginform.classList.toggle("active")
+})
+
+closeform.addEventListener('click',()=>{
+    loginform.classList.remove("active")
+})
+
+menubar.addEventListener('click',()=>{
+    menubar.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+})
+
